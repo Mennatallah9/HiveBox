db6b5994a95e13d37b8fc2f2533f5b6a977d00e7
Mennatallah9 <mennaa619@gmail.com>
2025-03-13 02:28:25 +0200

    add server.js file

--- app.js
+++ app.js
@@ -7,6 +7,4 @@ const app = new express();
 app.use('/temperature', tempRoutes);
 app.use('/version', versionRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+module.exports = app;
