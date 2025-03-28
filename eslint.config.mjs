b80c0d58da727346b598d9287b9941d615ae231f
Mennatallah9 <mennaa619@gmail.com>
2025-03-28 11:24:39 +0200

    Update ESLint configuration to include Node globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: { ...globals.browser, ...globals.node } }},
   pluginJs.configs.recommended,
   {ignores: ["**/tests/**/**.test.js"]}
 ];
